f81e4f79b905b7bd7f207b219f6b54663d4bd383
Vikas Malhotra <malhotra.vikas@gmail.com>
2024-03-21 11:54:28 -0400

    Added handling of Query Param

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [paramValue, setParamValue] = useState('');
+
+  useEffect(() => {
+    // Parse the query string from the current URL
+    const queryParams = new URLSearchParams(window.location.search);
+    const param = queryParams.get('param');
+    if (param) {
+      setParamValue(param);
+    }
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+        {paramValue && (
+          <p>Query parameter value is: {paramValue}</p>
+        )}
         <a
           className="App-link"
           href="https://reactjs.org"
